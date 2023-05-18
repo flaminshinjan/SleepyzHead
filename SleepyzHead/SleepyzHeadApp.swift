00918060f887dd7a492e149767df86a53300cf46
flaminshinjan <srikpatra.2008@gmail.com>
2023-05-18 11:31:28 +0530

    Started Work

---
+++ SleepyzHead/SleepyzHeadApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepyzHeadApp.swift
+//  SleepyzHead
+//
+//  Created by Shinjan Patra on 18/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SleepyzHeadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
